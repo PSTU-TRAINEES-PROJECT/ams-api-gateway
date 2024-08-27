8dbd5e25aad004fbfca9e7d6f2122be42f6b694e
i~am~mim <mehedi.mim.bd@gmail.com>
2024-08-27 15:50:02 +0600

    ams gateway made it runnable

---
+++ kong/plugins/gateway/schema.lua
@@ -0,0 +1,17 @@
+local typedefs = require "kong.db.schema.typedefs"
+
+return {
+  name = "ams-gateway",
+  fields = {
+    { consumer = typedefs.no_consumer },
+    { protocols = typedefs.protocols_http },
+    {
+      config = {
+        type = "record",
+        fields = {
+            {  header = { type = "string", required = true, default  = "Authorization" }, },
+        },
+      },
+    },
+  },
+}
